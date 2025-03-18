fd5e56e07236c9e884653a228f5b52dfcf18c972
Umar Ejaz <umar.senpaiiii@gmail.com>
2025-03-18 10:20:18 -0700

    v10

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -7,7 +7,7 @@ const CTA = () => {
   const locomotiveScroll = new LocomotiveScroll();
   return (
     <>
-      <div className="w-full h-screen flex flex-col items-center justify-center py-12 bg-white rounded-t-[170px]">
+      <div className="w-full h-auto flex flex-col items-center justify-center py-12 bg-white rounded-t-[170px]">
         <FaHandshake className="text-8xl text-gray-800" />
         <div className="info w-full md:w-1/2 px-4 text-4xl md:text-5xl mt-5 text-center font-medium bg-gradient-to-br from-[#1D1D1D] to-[#60605F] bg-clip-text text-transparent tracking-tighter leading-none mb-8">
           Tell me about your next project
